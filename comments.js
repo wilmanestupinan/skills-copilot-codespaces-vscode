b3a36f7cac868ab5323f8a41d9ec9516c7f96869
Wilman Estupiñan Molina <63934232+wilmanestupinan@users.noreply.github.com>
2024-09-19 15:32:30 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,1 +1,16 @@
-// create a new web server
+// Create web server and listen on port 3000
+// Use the comments.js file to handle requests to /comments path
+// The comments.js file should export a function that takes a request and response object as arguments
+
+var http = require("http");
+var comments = require("./comments");
+
+http
+  .createServer(function (req, res) {
+    if (req.url === "/comments") {
+      comments(req, res);
+    } else {
+      res.end("Not found");
+    }
+  })
+  .listen(3000);
